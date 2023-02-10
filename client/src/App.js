e70fb0293cae1f70f9ec4bd6cc26e18cfd62be61
hj5230 <hf305@foxmail.com>
2023-02-11 00:30:57 +0200

    commit

---
+++ client/src/App.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link, Routes, useParams } from 'react-router-dom';
+import './App.css';
+
+function Index() {
+  const [book, setBook] = useState({
+    name: "",
+    author: "",
+    pages: ""
+  })
+
+  const handleChange = e => {
+    setBook({ ...book, [e.target.name]: e.target.value });
+  };
+
+  const handleClick = async() => {
+    const pro = await fetch('/api/book/', {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'        
+      },
+      body: JSON.stringify(book)
+    })
+    const res = await pro.json()
+    console.log(res)
+  }
+
+  return (
+    <>
+    <h1>books</h1>
+    <input type="string" id="name" name="name" placeholder="name" value={book.name} onChange={handleChange} /><br />
+    <input type="string" id="author" name="author" placeholder="author" value={book.author} onChange={handleChange} /><br />
+    <input type="number" id="pages" name="pages" placeholder="pages" value={book.pages} onChange={handleChange} /><br />
+    <input type="submit" id="submit" onClick={handleClick} />
+    </>
+  )
+}
+
+function Book() {
+  const fetchData = async(bookName) => {
+    const pro = await fetch('/api/' + bookName)
+    const res = await pro.json()
+    setBookData(res)
+  }
+  const params = useParams();
+  const [bookData, setBookData] = useState(null)
+  useEffect(() => {
+    fetchData(params.bookName)
+  }, [params.bookName])
+
+  return (
+    <>
+    <h1>Books</h1>
+    {bookData ? (
+      bookData.notFound ? (
+        <h1>404: This is not the webpage you are looking for</h1>
+      ) : (
+        <>
+        <p>{bookData.name}</p>
+        <p>{bookData.author}</p>
+        <p>{bookData.pages}</p>
+        </>
+      )
+    ) : (
+      <p>loading..</p>
+    )}
+    </>
+  )
+}
+
+function App() {
+
+
+  return (
+    <>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Index</Link>
+        </nav>
+        <Routes>
+        <Route path="/" exact element={<Index />} />
+        <Route path="/book/:bookName" element={<Book />} />
+        </Routes>
+      </div>
+    </Router>
+
+    </>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { default: mongoose } = require("mongoose");
+const cors = require('cors');
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve("..", "client", "build")));
+  app.get("*", (req, res) => res.sendFile(path.resolve("..", "client", "build", "index.html")))
+} else if(process.env.NODE_ENV === 'development') {
+  var corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  };
+  app.use(corsOptions)
+}
+
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://localhost:27017/testdb", (err, client) => {
+  if (err) throw err;
+  if (client) console.log("---Mongodb Connected---\n");
+});
+
+const bookSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  pages: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+app.post('/api/book/', (req, res) => {
+  const { author, name, pages } = req.body;
+  new Book({
+    author: author,
+    name: name,
+    pages: pages,
+  }).save((err) => {
+    if (err) throw err;
+    res.json('ok');
+  });
+});
+
+app.get('/api/:name', (req, res) => {
+  const { name } = req.params;
+  Book.findOne({name: name}, (err, book) => {
+    if(err) throw err;
+    if(book) return res.json(book);
+    else return res.status(404).send({
+      notFound: true
+    });
+  });
+});
+
+module.exports = app;
